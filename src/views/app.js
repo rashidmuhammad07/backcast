40c307a8b217a6922349b4941b6adb1fa761a619
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-10-23 20:58:13 -0700

    partial work done

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,16 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.videoListView = new VideoListView(this.videos);
+    this.render();
   },
 
 
   render: function() {
+    console.log('hey');
     this.$el.html(this.template());
+    this.$el.append(this.videoListView.render().el);
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,17 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    
+  },
 
   render: function() {
+    console.log('hey');
     this.$el.children().detach();
     this.$el.html(this.template());
+    console.log(this);
+    
     return this;
+    
   },
 
   template: templateURL('src/templates/videoList.html')
